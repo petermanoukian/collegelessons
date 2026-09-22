09c2e14cc5345585cd97b2d991ecd5632d185374
Peter Manoukian <thewizard_it@hotmail.com>
2026-09-22 18:31:29 +0400

    upload from PC - Lesson 7 add the latest database

--- Lesson7/database/college.sql
+++ Lesson7/database/college.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 15, 2026 at 03:25 PM
+-- Generation Time: Sep 22, 2026 at 02:30 PM
 -- Server version: 8.4.3
 -- PHP Version: 8.5.1
 
@@ -38,6 +38,9 @@ CREATE TABLE `students` (
   `gender` enum('Male','Female') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Male',
   `membership` enum('Member','Full Member') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Member',
   `newsletter` tinyint(1) NOT NULL DEFAULT '0',
+  `img` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `thumb` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `file` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -45,11 +48,15 @@ CREATE TABLE `students` (
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`id`, `username`, `first_name`, `last_name`, `age`, `email`, `nickname`, `gender`, `membership`, `newsletter`, `created_at`) VALUES
-(1, 'user1', 'bedik', 'manoukian', 8, 'bedikmanoukian@gmail.com', 'N/A', 'Male', 'Full Member', 1, '2026-09-15 14:50:40'),
-(2, 'user2', 'uyuyt', 'uyuyuty', 25, 'bedikmanoukian@gmail.net', 'N/A', 'Male', 'Member', 1, '2026-09-15 15:02:46'),
-(3, 'user3', 'Peter', 'Manoukian', 7, 'thewizard_it@hotmail.com', 'N/A', 'Male', 'Member', 0, '2026-09-15 15:03:28'),
-(4, 'user4', 'hgfhfg', 'hgfhf', 8, 'test@test.com', 'N/A', 'Female', 'Full Member', 0, '2026-09-15 15:03:52');
+INSERT INTO `students` (`id`, `username`, `first_name`, `last_name`, `age`, `email`, `nickname`, `gender`, `membership`, `newsletter`, `img`, `thumb`, `file`, `created_at`) VALUES
+(1, 'user1', 'bedik', 'manoukian', 8, 'bedikmanoukian@gmail.com', 'N/A', 'Male', 'Full Member', 1, NULL, NULL, NULL, '2026-09-15 14:50:40'),
+(2, 'user2', 'namer', 'updatrer', 25, 'bedikmanoukian@gmail.net', 'N/A', 'Male', 'Member', 1, 'uploads/student/img/1790086369_6ab28ce14b20c.jpg', 'uploads/student/img/thumb/1790086369_6ab28ce14b20c.jpg', 'uploads/student/file/1790086369_doc_6ab28ce152724.jpg', '2026-09-15 15:02:46'),
+(3, 'user3', 'Peter', 'Manoukian', 7, 'thewizard_it@hotmail.com', 'N/A', 'Male', 'Member', 0, NULL, NULL, NULL, '2026-09-15 15:03:28'),
+(4, 'user4', 'hgfhfg', 'hgfhf', 8, 'test@test.com', 'N/A', 'Female', 'Full Member', 0, NULL, NULL, NULL, '2026-09-15 15:03:52'),
+(5, 'peterman', 'Bedros', 'Manoukian', 2, 'thewizard_it@hotmail.ws', 'nickname1', 'Male', 'Member', 1, NULL, NULL, NULL, '2026-09-16 15:37:20'),
+(6, 'test', 'test', 'testlast', 7, 'test@test.net', 'nickname1', 'Female', 'Full Member', 1, NULL, NULL, NULL, '2026-09-16 15:38:14'),
+(7, 'peterman2', 'Bedros', 'Manoukian', 4, 'thewizard_it@yahoo.com', 'nickname2', 'Female', 'Full Member', 1, 'uploads/student/img/1790086395_6ab28cfb9da9d.jpg', 'uploads/student/img/thumb/1790086395_6ab28cfb9da9d.jpg', 'uploads/student/file/1790086395_doc_6ab28cfba9def.docx', '2026-09-16 15:39:05'),
+(8, 'pic1', 'Bedros', 'Manoukian', 11, 'thewizard_it@google.com', 'gogole222', 'Male', 'Member', 0, 'uploads/student/img/1790083837_6ab282fdf1b82.jpg', 'uploads/student/img/thumb/1790083837_6ab282fdf1b82.jpg', 'uploads/student/file/1790083838_doc_6ab282fe07c56.pdf', '2026-09-22 13:30:38');
 
 --
 -- Indexes for dumped tables
@@ -73,7 +80,7 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
